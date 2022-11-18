87d481d59f2ed378dc4c6108e1558ff455b95e8b
hari <harikarumuri1086@gmail.com>
2022-11-18 19:19:32 +0530

    lorem

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -3,7 +3,7 @@ import React from 'react'
 const home = () => {
   return (
     <>
-      <h3>this is home page</h3>
+      <h3>this is home page Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis hic distinctio ad. Tempora dicta impedit eius maxime laborum nesciunt necessitatibus labore, vitae et odio assumenda dolore, praesentium fugit illo! Voluptate rem commodi facilis adipisci. Dolores, consequuntur eos. Assumenda totam asperiores doloribus maiores dicta praesentium ullam. Nam ipsam perspiciatis eos suscipit, iure provident nostrum at magnam a dicta rem voluptatum omnis reiciendis! Quibusdam labore recusandae itaque reiciendis necessitatibus delectus iusto, porro at distinctio in voluptatum facere suscipit, minus asperiores eveniet. Mollitia tenetur qui rerum architecto ratione repellendus quisquam harum aut excepturi necessitatibus voluptatibus, provident, alias adipisci ducimus, minus est illo dolor iste aliquid. Facilis nulla fuga accusamus atque minus, non nostrum perspiciatis facere laudantium cupiditate quod officiis iure, sequi veritatis, consequuntur alias hic voluptas. Excepturi error sapiente voluptates perferendis ad consequatur. Aperiam animi cumque et voluptates accusantium debitis veniam impedit. Ratione quibusdam consequatur aliquid ullam voluptatum, magni velit maiores similique quaerat, minus nobis odit laboriosam reiciendis placeat distinctio veniam quisquam cupiditate et, soluta nostrum a eligendi. Architecto, labore earum libero cupiditate vel consequuntur, molestiae, nobis laborum saepe quod dolorem fuga et! Perspiciatis, aperiam, officia neque esse alias iusto nam repellendus cumque distinctio ipsa tenetur laudantium! Libero quisquam at labore esse aliquam.</h3>
     </>
   )
 }
